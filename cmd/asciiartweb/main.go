dd3e23e10d6f833d43ae92fac44da6a6ba3aecdf
hasabdulla <habdulla2903@gmail.com>
2023-09-29 18:34:16 +0300

    hasan

--- cmd/asciiartweb/main.go
+++ cmd/asciiartweb/main.go
@@ -13,17 +13,18 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful!")
 	name := r.FormValue("name")       // value to be changed to what input side name identifier has
-	address := r.FormValue("address") // value of output?
+	// address := r.FormValue("address") // value of output?
 	fmt.Fprintf(w, "name = %s\n", name)
-	fmt.Fprintf(w, "address = %s\n", address)
+	// fmt.Fprintf(w, "address = %s\n", address)
+	fmt.Println(name)
+	// fmt.Printf( "address = %s\n", address)
 	asciiart.AsciiArt()
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.StatusNotFound)
+		http.Error(w, "404 not found dumbass", http.StatusNotFound)
 		return
 	}
 	if r.Method != "GET" {
@@ -34,7 +35,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fileserver := http.FileServer(http.Dir("./static"))
+	fileserver := http.FileServer(http.Dir("../../static"))
 	http.Handle("/", fileserver)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
