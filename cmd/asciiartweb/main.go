c2b2acd3ed3651294a109b97b81430f640b5d1f4
rjamal <rami@piercer.gg>
2023-10-29 00:43:53 +0300

    RJ

--- cmd/asciiartweb/main.go
+++ cmd/asciiartweb/main.go
@@ -2,80 +2,40 @@ package main
 
 import (
 	"fmt"
-	//"html/template"
+	"html/template"
 	"log"
 	"net/http"
-	"text/template"
 
 	"asciiartweb/internal/asciiart"
+	"asciiartweb/internal/asciiartfs"
 )
-
-var temp1 *template.Template
-
-func init() {
-	temp1 = template.Must(template.ParseFiles("../../static/404.html"))
-}
 
 type Fonts struct {
 	Art    string
 	Hidden string
 }
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	name := r.FormValue("name")
-	banner := r.FormValue("banner")
-	fmt.Println("banner= " + banner)
-	art := asciiart.AsciiArt(banner, name)
-	fonts := Fonts{Art: art, Hidden: "false"}
-	parsedTemplate, err := template.ParseFiles("../../static/index.html")
-	if err != nil {
-		log.Println("Error executing template :", err)
-		return
-	}
-
-	err = parsedTemplate.Execute(w, fonts)
-	if err != nil {
-		log.Println("Error executing template :", err)
-		return
-	}
-	// err = parsedTemplate.Execute(w, fonts)
-	// if err != nil {
-	// 	log.Println("Error executing template :", err)
-	// 	return
-	// }
-
-	// if name == "" || name >= string(32) || name <= string(128) {
-	// 	http.ServeFile(w, r, "../../static/400.html")
-	// 	return
-	// }
-	if r.URL.Path != "/" && r.URL.Path != "/ascii-art" {
-		helloHandler(w, r)
-	}
-}
-
-func hiHandler(w http.ResponseWriter, r *http.Request) {
-	parsedTemplate, _ := template.ParseFiles("../../static/400.html")
-	parsedTemplate.Execute(w, r)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	temp1.ExecuteTemplate(w, "404.html", r)
-	w.WriteHeader(404)
-	parsedTemplate, _ := template.ParseFiles("../../static/404.html")
-	parsedTemplate.Execute(w, nil)
-}
-
-func errorHandler(w http.ResponseWriter, r *http.Request) {
-	parsedTemplate, _ := template.ParseFiles("../../static/500.html")
-	parsedTemplate.Execute(w, nil)
-}
-
 func printHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		parsedTemplate, _ := template.ParseFiles("../../static/405.html")
+		parsedTemplate.Execute(w, nil)
+		return
+	}
 
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		parsedTemplate, _ := template.ParseFiles("../../static/404.html")
+		parsedTemplate.Execute(w, nil)
+		return
+	}
+
+	// if  {
+	// http.Error(w, " ", http.StatusMethodNotAllowed)
+	// parsedTemplate, _ := template.ParseFiles("../../static/405.html")
+	// parsedTemplate.Execute(w, nil)
+	// return
+	// }
 	parsedTemplate, err := template.ParseFiles("../../static/index.html")
 	if err != nil {
 		log.Println("Error executing template :", err)
@@ -87,19 +47,67 @@ func printHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println("Error executing template :", err)
 		return
 	}
+}
 
-	// if r.URL.Path != "/" {
-	// 	return
-	// }
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	// if r.URL.Path != "/ascii-art" {
+	// 	w.WriteHeader(http.StatusNotFound)
+	// 	parsedTemplate, _ := template.ParseFiles("../../static/404.html")
+	// 	parsedTemplate.Execute(w, nil)
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		parsedTemplate, _ := template.ParseFiles("../../static/405.html")
+		parsedTemplate.Execute(w, nil)
+		return
+	} else {
+		name := r.FormValue("name")
+		errName := asciiartfs.IsValid(name)
+		if errName != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			parsedTemplate, _ := template.ParseFiles("../../static/400.html")
+			err := parsedTemplate.Execute(w, nil)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				parsedTemplate, _ := template.ParseFiles("../../static/500.html")
+				err = parsedTemplate.Execute(w, nil)
+				if err != nil {
+					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				}
+			}
+			return
+		}
+		banner := r.FormValue("banner")
+		// errbann := asciiart.AsciiArt(banner, name)
+		if banner != "Shadow" && banner != "standard" && banner != "thinkertoy" {
+			w.WriteHeader(http.StatusBadRequest)
+			parsedTemplate, _ := template.ParseFiles("../../static/404.html")
+			parsedTemplate.Execute(w, nil)
+			return
+		}
+		art := asciiart.AsciiArt(banner, name)
+		fonts := Fonts{Art: art, Hidden: "false"}
+		parsedTemplate, err := template.ParseFiles("../../static/index.html")
+		if err != nil {
+			log.Println("Error executing template :", err)
+			return
+		}
+
+		err = parsedTemplate.Execute(w, fonts)
+		if err != nil {
+			log.Println("Error executing template :", err)
+			return
+		}
+	}
 
 }
 
 func main() {
 	http.HandleFunc("/", printHandler)
 	http.HandleFunc("/ascii-art", formHandler)
-	http.HandleFunc("/400", hiHandler) // 400 bad request
-	http.HandleFunc("/404", helloHandler)
-	http.HandleFunc("/500", errorHandler)
 	http.HandleFunc("/w.css", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "../../static/w.css")
 	})
